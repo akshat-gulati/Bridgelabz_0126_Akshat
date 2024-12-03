d2f703ef9db3bad0ab2197775263e2b07f1fffe9
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-12-03 14:09:45 +0530

    Akshat Add practice data to the jsAdv.js and updated readme

--- jsAdvancedBasic.js
+++ jsAdvancedBasic.js
@@ -69,3 +69,121 @@ var obj2 = {...obj}
 console.log(obj2);
 
 6. //Truthy and Falsy
+// falsy values: 0, false, undefined, null, NaN, document.all
+// truthy values: remaining values are truthy
+
+7. //switch case
+switch (1) {
+    case 1:
+        console.log("this is one");
+        break;
+
+    default:
+        console.log("this id default");
+        break;
+};
+
+8. //foreach, forin, do-while
+// foreach works on array only
+
+var a = [63, 727, 8, 778, 89, 94, 2, 890, 6543, 100]
+
+a.forEach(function(val) {
+    console.log(val+2);
+})
+
+// for-in used to loop on objects
+
+var obj = {
+    name: "harsh",
+    age: 24,
+    city: "Delhi"
+}
+
+for (var key in obj) {
+    console.log(obj[key]);
+}
+
+// do-while loop
+
+var a = 12;
+do {
+    console.log(a++); // This will work for atleast one.
+} while (a<11);
+
+9. // Callback function
+// A callback function in JavaScript is a function that is passed as an argument to another function and is executed after some operation has been completed. This is particularly useful for asynchronous operations, like fetching data from a server.
+setTimeout(function() {
+    console.log("2 sec bad");
+}, 2000);
+
+
+// Define a function that takes a callback
+function greet(name, callback) {
+    console.log('Hello ' + name);
+    callback();
+}
+
+// Define a callback function
+function sayGoodbye() {
+    console.log('Goodbye!');
+}
+
+// Call the greet function with the callback
+greet('Alice', sayGoodbye);
+
+
+
+function fetchData(callback) {
+    setTimeout(() => {
+        console.log('Data fetched');
+        callback();
+    }, 2000);
+}
+
+function processData() {
+    console.log('Processing data');
+}
+
+fetchData(processData);
+
+10. //First class function
+
+// in js there is a concept fns ko use kr skte ho as a value. simply fn can be treated as values
+
+var a = function(){}
+
+function abc(a) {
+    a();
+}
+
+abc(function(){
+    console.log("Hello");
+})
+
+11. //array is a type of object in js, below is how it is implemented
+
+var arr = [1,2,3,4,5]
+
+// arr = { //index, number
+//     0:1,
+//     1:2
+// }
+console.log(arr);
+
+arr[-1] = 45
+
+console.log(arr);
+
+console.log(Array.isArray([]))
+console.log(Array.isArray({}))
+
+12. //How to delete Object prop
+
+var a = {
+    name: "Akshat",
+    age: 24
+}
+
+delete a.age
+console.log(a);
