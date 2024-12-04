c62f75169d5cccecfb961ec7bbe50eb10d199eb6
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-12-04 10:35:37 +0530

    Akshat Update EmployeeWages.js to task UC1

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,6 +1,19 @@
+1. // Ability to Check Employee is present or Absent
+
 function calculateAttendance() {
-    let attendance = Math.floor(Math.random() * 10)
+    let attendance = Math.floor(Math.random() * 11)
     console.log(attendance);
+
+    if (attendance > 0.5) {
+        var status = "Present"
+    }
+    else{
+        var status = "Absent"
+    }
+
+    return status;
     
 }
-calculateAttendance()
+let ans = calculateAttendance()
+
+console.log(ans);
